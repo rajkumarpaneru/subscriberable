c3013122186578a6d6c7d2970b30723f616265fc
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-10-03 08:45:47 +0545

    service provider skeleton

---
+++ src/SubscriberableServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Raajkumarpaneru\Subscriberable;
+
+use Illuminate\Support\ServiceProvider;
+
+class SubscriberableServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
