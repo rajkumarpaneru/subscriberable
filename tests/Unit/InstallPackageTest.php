953e0dadd9659280f48731c4dee325fda7c44bc5
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-10-04 21:06:18 +0545

    tests to check for confirmation on config file overrride

--- tests/Unit/InstallPackageTest.php
+++ tests/Unit/InstallPackageTest.php
@@ -21,4 +21,64 @@ class InstallPackageTest extends TestCase
 
         $this->assertTrue(File::exists(config_path('subscriberable.php')));
     }
+
+
+    /** @test */
+    public function when_a_config_file_is_present_users_can_choose_to_not_overwrite_it()
+    {
+        // Given we have already have an existing config file
+        File::put(config_path('subscriberable.php'), 'test contents');
+        $this->assertTrue(File::exists(config_path('subscriberable.php')));
+
+        // When we run the install command
+        $command = $this->artisan('subscriberable:install');
+
+        // We expect a warning that our configuration file exists
+        $command->expectsConfirmation(
+            'Config file already exists. Do you want to overwrite it?',
+            // When answered with "no"
+            'no'
+        );
+
+        // We should see a message that our file was not overwritten
+        $command->expectsOutput('Existing configuration was not overwritten');
+
+        // Assert that the original contents of the config file remain
+        $this->assertEquals('test contents', file_get_contents(config_path('subscriberable.php')));
+
+        // Clean up
+        unlink(config_path('subscriberable.php'));
+    }
+
+    /** @test */
+    public function when_a_config_file_is_present_users_can_choose_to_do_overwrite_it()
+    {
+        // Given we have already have an existing config file
+        File::put(config_path('subscriberable.php'), 'test contents');
+        $this->assertTrue(File::exists(config_path('subscriberable.php')));
+
+        // When we run the install command
+        $command = $this->artisan('subscriberable:install');
+
+        // We expect a warning that our configuration file exists
+        $command->expectsConfirmation(
+            'Config file already exists. Do you want to overwrite it?',
+            // When answered with "yes"
+            'yes'
+        );
+
+        // execute the command to force override
+        $command->execute();
+
+        $command->expectsOutput('Overwriting configuration file...');
+
+        // Assert that the original contents are overwritten
+        $this->assertEquals(
+            file_get_contents(__DIR__.'/../config/config.php'),
+            file_get_contents(config_path('subscriberable.php'))
+        );
+
+        // Clean up
+        unlink(config_path('subscriberable.php'));
+    }
 }
