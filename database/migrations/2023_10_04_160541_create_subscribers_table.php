1c0fd806d6c7f94d8d9393ac5f3135f6391b8f73
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-10-04 21:56:55 +0545

    model and migration for subscribers

---
+++ database/migrations/2023_10_04_160541_create_subscribers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscribersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('subscriberable_id');
+            $table->string('subscriberable_type');
+            $table->unsignedBigInteger('subscriber_id');
+            $table->string('subscriber_type');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->boolean('status');
+            $table->string('payment_type');
+            $table->unsignedBigInteger('amount_paid');
+            $table->unsignedBigInteger('subscription_type_id');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscribers');
+    }
+}
