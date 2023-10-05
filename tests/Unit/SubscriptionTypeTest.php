0f9fc5e761db07d49de1d0f3fa147b1c13aeca82
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-10-05 21:14:46 +0545

    test to check for unique subscription name

--- tests/Unit/SubscriptionTypeTest.php
+++ tests/Unit/SubscriptionTypeTest.php
@@ -2,6 +2,7 @@
 
 namespace Raajkumarpaneru\Subscriberable\Tests\Unit;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Raajkumarpaneru\Subscriberable\Models\SubscriptionType;
 use Raajkumarpaneru\Subscriberable\Tests\TestCase;
@@ -19,6 +20,33 @@ class SubscriptionTypeTest extends TestCase
             'is_active' => 1
         ]);
         $this->assertEquals('monthly', $subscription_type->name);
+    }
+
+    /** @test */
+    function a_subscription_name_is_unique()
+    {
+        $subscription_name = 'monthly';
+
+        $existing_type = SubscriptionType::create([
+            'name' => $subscription_name,
+            'rank' => 1,
+            'is_active' => 1
+        ]);
+
+        try {
+            $new_type = SubscriptionType::create([
+                'name' => $subscription_name,
+                'rank' => 2,
+                'is_active' => 0
+            ]);
+
+            // If no exception is thrown, fail the test
+            $this->fail('Integrity constraint violation exception was not thrown.');
+        } catch (QueryException $e) {
+            $this->assertStringContainsString('Integrity constraint violation', $e->getMessage());
+        }
+
+        $this->assertDatabaseCount('subscription_types', 1);
     }
 
 }
