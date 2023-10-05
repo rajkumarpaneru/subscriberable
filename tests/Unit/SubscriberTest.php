2d7484bb7d052a6424c768fb23afc7e1cb47a7a2
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-10-05 19:17:39 +0545

    subscribers crud unit tests

---
+++ tests/Unit/SubscriberTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Raajkumarpaneru\Subscriberable\Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Raajkumarpaneru\Subscriberable\Tests\TestCase;
+use Raajkumarpaneru\Subscriberable\Models\Subscriber;
+
+class SubscriberTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function test_can_create_subscriber()
+    {
+        $subscriber = factory(Subscriber::class)->create();
+        $this->assertInstanceOf(Subscriber::class, $subscriber);
+        $this->assertDatabaseHas('subscribers', $subscriber->toArray());
+    }
+
+    /** @test */
+    public function test_can_read_subscriber()
+    {
+        $subscriber = factory(Subscriber::class)->create();
+        $this->assertInstanceOf(Subscriber::class, $subscriber);
+
+        $found = Subscriber::find($subscriber->id);
+        $this->assertEquals($found->id, $subscriber->id);
+    }
+
+    /** @test */
+    public function test_can_update_subscriber()
+    {
+        $subscriber = factory(Subscriber::class)->create();
+        $this->assertInstanceOf(Subscriber::class, $subscriber);
+
+        $updatedData = [
+            'status' => false,
+            'payment_type' => 'Credit Card',
+        ];
+
+        $subscriber->update($updatedData);
+        $this->assertDatabaseHas('subscribers', $updatedData);
+    }
+
+    /** @test */
+    public function test_can_delete_subscriber()
+    {
+        $subscriber = factory(Subscriber::class)->create();
+        $this->assertInstanceOf(Subscriber::class, $subscriber);
+
+        $subscriber->delete();
+        $this->assertDeleted($subscriber);
+    }
+}
